2f3a01ef3f5256d4373a6183d418e9655fbc7959
maxcoulombe <max.coulombe@hashicorp.com>
2022-09-06 12:05:10 -0400

    * switched to static-role support only

--- bootstrap/terraform/elasticache.tf
+++ bootstrap/terraform/elasticache.tf
@@ -7,19 +7,38 @@ provider "aws" {
   // region = ""
 }
 
+resource "random_password" "vault_plugin_elasticache_test" {
+  length = 16
+}
+
 resource "aws_elasticache_replication_group" "vault_plugin_elasticache_test" {
   replication_group_id       = "vault-plugin-elasticache-test"
   description                = "vault elasticache plugin generated test cluster"
-  engine                     = "redis"
+  engine                     = "REDIS"
   engine_version             = "6.2"
   node_type                  = "cache.t4g.micro"
   num_cache_clusters         = 1
   parameter_group_name       = "default.redis6.x"
   transit_encryption_enabled = true
+  user_group_ids             = [aws_elasticache_user_group.vault_plugin_elasticache_test.id]
 
   tags = {
     "description" : "vault elasticache plugin generated test cluster"
   }
+}
+
+resource "aws_elasticache_user_group" "vault_plugin_elasticache_test" {
+  engine        = "REDIS"
+  user_group_id = "vault-test-user-group"
+  user_ids      = ["default", aws_elasticache_user.vault_plugin_elasticache_test.user_id]
+}
+
+resource "aws_elasticache_user" "vault_plugin_elasticache_test" {
+  user_id       = "vault-test"
+  user_name     = "vault-test"
+  access_string = "on ~* +@all"
+  engine        = "REDIS"
+  passwords     = [random_password.vault_plugin_elasticache_test.result]
 }
 
 resource "aws_iam_user" "vault_plugin_elasticache_test" {
@@ -102,4 +121,9 @@ output "url" {
 data "aws_region" "current" {}
 output "region" {
   value = data.aws_region.current.name
+}
+
+// export TEST_ELASTICACHE_USER=${user}
+output "user" {
+  value = aws_elasticache_user.vault_plugin_elasticache_test.user_name
 }
--- redis_elasticache_client_test.go
+++ redis_elasticache_client_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"os"
 	"reflect"
-	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/elasticache"
@@ -31,17 +30,18 @@ type testCases []struct {
 	wantErr bool
 }
 
-func skipIfEnvIsUnset(t *testing.T, config config) {
-	if config.Username == "" || config.Password == "" || config.Url == "" || config.Region == "" {
+func skipIfEnvIsUnset(t *testing.T, config config, username string) {
+	if config.Username == "" || config.Password == "" || config.Url == "" || config.Region == "" || username == "" {
 		t.Skip("Skipping acceptance tests because required environment variables are not configured")
 	}
 }
 
-func setUpEnvironment() (fields, map[string]interface{}, redisElastiCacheDB) {
+func setUpEnvironment() (fields, map[string]interface{}, redisElastiCacheDB, string) {
 	username := os.Getenv("TEST_ELASTICACHE_USERNAME")
 	password := os.Getenv("TEST_ELASTICACHE_PASSWORD")
 	url := os.Getenv("TEST_ELASTICACHE_URL")
 	region := os.Getenv("TEST_ELASTICACHE_REGION")
+	user := os.Getenv("TEST_ELASTICACHE_USER")
 
 	f := fields{
 		logger: hclog.New(&hclog.LoggerOptions{
@@ -71,7 +71,7 @@ func setUpEnvironment() (fields, map[string]interface{}, redisElastiCacheDB) {
 		client: f.client,
 	}
 
-	return f, c, r
+	return f, c, r, user
 }
 
 func setUpClient(t *testing.T, r *redisElastiCacheDB, config map[string]interface{}) {
@@ -84,37 +84,9 @@ func setUpClient(t *testing.T, r *redisElastiCacheDB, config map[string]interfac
 	}
 }
 
-func setUpTestUser(t *testing.T, r *redisElastiCacheDB) string {
-	user, err := r.NewUser(nil, dbplugin.NewUserRequest{
-		UsernameConfig: dbplugin.UsernameMetadata{
-			DisplayName: "display",
-			RoleName:    "role",
-		},
-		Statements: dbplugin.Statements{
-			Commands: []string{"[\"~test*\",\"-@all\",\"+@read\"]"},
-		},
-		Password: "abcdefghijklmnopqrstuvwxyz",
-	})
-	if err != nil {
-		t.Errorf("unable to provision test user for test cases: %v", err)
-	}
-
-	return user.Username
-}
-
-func teardownTestUser(r redisElastiCacheDB, username string) {
-	if username == "" {
-		return
-	}
-
-	_, _ = r.DeleteUser(nil, dbplugin.DeleteUserRequest{
-		Username: username,
-	})
-}
-
 func Test_redisElastiCacheDB_Initialize(t *testing.T) {
-	f, c, r := setUpEnvironment()
-	skipIfEnvIsUnset(t, f.config)
+	f, c, r, u := setUpEnvironment()
+	skipIfEnvIsUnset(t, f.config, u)
 
 	tests := testCases{
 		{
@@ -163,117 +135,11 @@ func Test_redisElastiCacheDB_Initialize(t *testing.T) {
 	}
 }
 
-func Test_redisElastiCacheDB_NewUser(t *testing.T) {
-	f, c, r := setUpEnvironment()
-
-	skipIfEnvIsUnset(t, f.config)
-
-	setUpClient(t, &r, c)
-
-	tests := testCases{
-		{
-			name:   "create new valid user succeeds",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.NewUserRequest{
-					UsernameConfig: dbplugin.UsernameMetadata{
-						DisplayName: "display",
-						RoleName:    "role",
-					},
-					Statements: dbplugin.Statements{
-						Commands: []string{"[\"~test*\",\"-@all\",\"+@read\"]"},
-					},
-					Password: "abcdefghijklmnopqrstuvwxyz",
-				},
-			},
-			want: dbplugin.NewUserResponse{
-				Username: "v_displ_role_",
-			},
-		},
-		{
-			name:   "create user truncates username",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.NewUserRequest{
-					UsernameConfig: dbplugin.UsernameMetadata{
-						DisplayName: "iAmSupeExtremelyLongThisWillHaveToBeTruncated",
-						RoleName:    "iAmEvenLongerTheApiWillDefinitelyRejectUsIfWeArePassedAsIsWithoutAnyModifications",
-					},
-					Statements: dbplugin.Statements{
-						Commands: []string{"[\"~test*\",\"-@all\",\"+@read\"]"},
-					},
-					Password: "abcdefghijklmnopqrstuvwxyz",
-				},
-			},
-			want: dbplugin.NewUserResponse{
-				Username: "v_iAmSu_iAmEvenLongerTheApiWillDefinitelyRejec",
-			},
-		},
-		{
-			name:   "create user with invalid password fails",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.NewUserRequest{
-					UsernameConfig: dbplugin.UsernameMetadata{
-						DisplayName: "display",
-						RoleName:    "role",
-					},
-					Statements: dbplugin.Statements{
-						Commands: []string{"[\"~test*\",\"-@all\",\"+@read\"]"},
-					},
-					Password: "too short",
-				},
-			},
-			want:    dbplugin.NewUserResponse{},
-			wantErr: true,
-		},
-		{
-			name:   "create user with invalid statements fails",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.NewUserRequest{
-					UsernameConfig: dbplugin.UsernameMetadata{
-						DisplayName: "display",
-						RoleName:    "role",
-					},
-					Statements: dbplugin.Statements{
-						Commands: []string{"+@invalid"},
-					},
-					Password: "abcdefghijklmnopqrstuvwxyz",
-				},
-			},
-			want:    dbplugin.NewUserResponse{},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := r.NewUser(tt.args.ctx, tt.args.req.(dbplugin.NewUserRequest))
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewUser() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !strings.HasPrefix(got.Username, tt.want.(dbplugin.NewUserResponse).Username) {
-				t.Errorf("NewUser() got = %v, want %v", got, tt.want)
-			}
-
-			teardownTestUser(r, got.Username)
-		})
-	}
-}
-
 func Test_redisElastiCacheDB_UpdateUser(t *testing.T) {
-	f, c, r := setUpEnvironment()
+	f, c, r, u := setUpEnvironment()
 
-	skipIfEnvIsUnset(t, f.config)
-
+	skipIfEnvIsUnset(t, f.config, u)
 	setUpClient(t, &r, c)
-	username := setUpTestUser(t, &r)
-	defer teardownTestUser(r, username)
 
 	tests := testCases{
 		{
@@ -282,7 +148,7 @@ func Test_redisElastiCacheDB_UpdateUser(t *testing.T) {
 			args: args{
 				ctx: context.Background(),
 				req: dbplugin.UpdateUserRequest{
-					Username:       username,
+					Username:       u,
 					CredentialType: 0,
 					Password: &dbplugin.ChangePassword{
 						NewPassword: "abcdefghijklmnopqrstuvwxyz1",
@@ -313,7 +179,7 @@ func Test_redisElastiCacheDB_UpdateUser(t *testing.T) {
 			args: args{
 				ctx: context.Background(),
 				req: dbplugin.UpdateUserRequest{
-					Username:       username,
+					Username:       u,
 					CredentialType: 0,
 					Password: &dbplugin.ChangePassword{
 						NewPassword: "too short",
@@ -333,175 +199,6 @@ func Test_redisElastiCacheDB_UpdateUser(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("UpdateUser() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_redisElastiCacheDB_DeleteUser(t *testing.T) {
-	f, c, r := setUpEnvironment()
-
-	skipIfEnvIsUnset(t, f.config)
-
-	setUpClient(t, &r, c)
-	username := setUpTestUser(t, &r)
-
-	tests := testCases{
-		{
-			name:   "delete existing user succeeds",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.DeleteUserRequest{
-					Username: username,
-				},
-			},
-			want:    dbplugin.DeleteUserResponse{},
-			wantErr: false,
-		},
-		{
-			name:   "delete non-existing user is lenient",
-			fields: f,
-			args: args{
-				ctx: context.Background(),
-				req: dbplugin.DeleteUserRequest{
-					Username: "I do not exist",
-				},
-			},
-			want: dbplugin.DeleteUserResponse{},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := r.DeleteUser(tt.args.ctx, tt.args.req.(dbplugin.DeleteUserRequest))
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DeleteUser() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("DeleteUser() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_parseCreationCommands(t *testing.T) {
-	type testCases []struct {
-		name     string
-		commands []string
-		want     string
-		wantErr  bool
-	}
-
-	tests := testCases{
-		{
-			name:     "empty command returns read-only user",
-			commands: []string{},
-			want:     "on ~* +@read",
-		},
-		{
-			name:     "single command with multiple rules parses correctly",
-			commands: []string{"[\"~test*\",\"-@all\",\"+@read\"]"},
-			want:     "on ~test* -@all +@read",
-		},
-		{
-			name:     "multiple commands with multiple rules parses correctly",
-			commands: []string{"[\"~test*\"]", "[\"-@all\", \"+@read\"]"},
-			want:     "on ~test* -@all +@read",
-		},
-		{
-			name:     "empty commands are tolerated",
-			commands: []string{"[\"~test*\"]", "[]", "[\"-@all\", \"+@read\"]"},
-			want:     "on ~test* -@all +@read",
-		},
-		{
-			name:     "'on' is added if missing for convenience",
-			commands: []string{"[\"~test*\"]"},
-			want:     "on ~test*",
-		},
-		{
-			name:     "'on' is ignored if passed at the beginning",
-			commands: []string{"[\"on\", \"~test*\"]"},
-			want:     "on ~test*",
-		},
-		{
-			name:     "'on' is ignored if passed explicitly within the rules",
-			commands: []string{"[\"~test*\", \"on\"]", "[\"+@read\"]"},
-			want:     "~test* on +@read",
-		},
-		{
-			name:     "'on' is ignored if passed explicitly at the end",
-			commands: []string{"[\"~test*\", \"on\"]"},
-			want:     "~test* on",
-		},
-		{
-			name:     "parsing invalid command format fails",
-			commands: []string{"{\"command:\", \"on ~* +@read\"}"},
-			want:     "",
-			wantErr:  true,
-		},
-		{
-			name:     "creation of disabled users is forbidden",
-			commands: []string{"[\"~test*\", \"off\"]", "[\"+@read\"]"},
-			want:     "",
-			wantErr:  true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseCreationCommands(tt.commands)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("parseCreationCommands() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("parseCreationCommands() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_normaliseId(t *testing.T) {
-	type args struct {
-		username string
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "compliant username",
-			args: args{username: "isrole1234eEvyH4mEPcCIT4tCvE131660656371"},
-			want: "isrole1234eEvyH4mEPcCIT4tCvE131660656371",
-		},
-		{
-			name: "short username",
-			args: args{username: "abcd"},
-			want: "abcd",
-		},
-		{
-			name: "username too long",
-			args: args{username: "vtokenredisrole1234eEvyH4mEPcCIT4tCvE131660656371"},
-			want: "isrole1234eEvyH4mEPcCIT4tCvE131660656371",
-		},
-		{
-			name: "username with non-alphanumeric characters",
-			args: args{username: "v_token_redis-role!/$}"},
-			want: "vtokenredis-role",
-		},
-		{
-			name: "username starting with a number",
-			args: args{username: "1bcd"},
-			want: "abcd",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := normaliseId(tt.args.username); got != tt.want {
-				t.Errorf("generateUserId() = %v, want %v", got, tt.want)
 			}
 		})
 	}
